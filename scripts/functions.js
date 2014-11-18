732e497db0746b364833eab0ee055f677dd281b3
Hervé Torche <herve.torche@fbw-one.com>
2014-11-18 09:38:36 +0100

    test

--- scripts/functions.js
+++ scripts/functions.js
@@ -85,24 +85,31 @@ function classement(division, success){
 }
 
 function test(){
-  var $cont = $('<tr><td><img src="http://www.fshbr.ch/img/clubs/murist-montet-black-tigers-hover.png"><a href="http://www.fshbr.ch/equipes/equipe/50">Murist/Montet Black Tigers II</a></td><td>2</td><td>1</td><td>0</td><td>1</td><td>12</td><td>13</td><td>-1</td><td>2</td></tr>');
+  var $cont = $('<table><tr><td><img src="http://www.fshbr.ch/img/clubs/murist-montet-black-tigers-hover.png"><a href="http://www.fshbr.ch/equipes/equipe/50">Murist/Montet Black Tigers II</a></td><td>2</td><td>1</td><td>0</td><td>1</td><td>12</td><td>13</td><td>-1</td><td>2</td></tr><tr><td><img src="http://www.fshbr.ch/img/clubs/murist-montet-black-tigers-hover.png"><a href="http://www.fshbr.ch/equipes/equipe/50">Murist/Montet Black Tigers II</a></td><td>2</td><td>1</td><td>0</td><td>1</td><td>12</td><td>13</td><td>-1</td><td>2</td></tr></table>');
   var $liste = $("#page_classement .classement tbody");
 
   $liste.empty();
 
-  equipe = {};
-  $scores = $cont.find("td");
-  equipe.nom        = $($scores[0]).text();
-  equipe.joue       = $($scores[1]).text();
-  equipe.gagne      = $($scores[2]).text();
-  equipe.egalite    = $($scores[3]).text();
-  equipe.perdu      = $($scores[4]).text();
-  equipe.marques    = $($scores[5]).text();
-  equipe.encaisses  = $($scores[6]).text();
-  equipe.difference = $($scores[7]).text();
-  equipe.points     = $($scores[8]).text();
+  var $equipes = $cont.find("tr");
 
-  $liste.append("<tr class='equipe'><td>" + equipe.nom + "</td><td>" + equipe.joue + "</td><td>" + equipe.gagne + "</td><td>" + equipe.egalite + "</td><td>" + equipe.perdu + "</td><td>" + equipe.marques + "</td><td>" + equipe.encaisses + "</td><td>" + equipe.difference + "</td><td>" + equipe.points + "</td></tr>")
+  console.log($equipes);
+
+  $equipes.each(function(){
+    var $el = $(this);
+    equipe = {};
+    $scores = $el.find("td");
+    equipe.nom        = $($scores[0]).text();
+    equipe.joue       = $($scores[1]).text();
+    equipe.gagne      = $($scores[2]).text();
+    equipe.egalite    = $($scores[3]).text();
+    equipe.perdu      = $($scores[4]).text();
+    equipe.marques    = $($scores[5]).text();
+    equipe.encaisses  = $($scores[6]).text();
+    equipe.difference = $($scores[7]).text();
+    equipe.points     = $($scores[8]).text();
+    
+    $liste.append("<tr class='equipe'><td>" + equipe.nom + "</td><td>" + equipe.joue + "</td><td>" + equipe.gagne + "</td><td>" + equipe.egalite + "</td><td>" + equipe.perdu + "</td><td>" + equipe.marques + "</td><td>" + equipe.encaisses + "</td><td>" + equipe.difference + "</td><td>" + equipe.points + "</td></tr>")
+  });
 }
 
 
